2eed7a13e20e59b605bd0cc24cfcc064793159e3
SuboshkA <zhanusakov_s@auca.kg>
2024-11-19 12:56:14 +0600

    Created backend request

---
+++ src/components/ProductCard/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+export function ProductCard(props){
+    const {image, category, title, price}= props.data;
+
+    return(
+        <div className='product-card'>
+            <img width="200" src={image} alt="" />
+            <div>{category}</div>
+            <h4>{title}</h4>
+            <div>{price}</div>
+        </div>
+    )
+}
